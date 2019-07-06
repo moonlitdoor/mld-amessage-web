45e95986faad275d7f19143613cb90da629da83d
Brian Amesbury <bamesbury@sofi.org>
2019-07-06 08:50:59 -0600

    function sends the message to fcm

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+import * as admin from 'firebase-admin';
+import Message = admin.messaging.Message;
+
+let app = admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+});
+
 export const gcm = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    if ("POST" === request.method) {
+        let body = JSON.parse(request.rawBody.toString()) as Message;
+        app.messaging().send(body)
+            .then((res) => {
+                response.send(res);
+            })
+            .catch((error) => {
+                console.log(error);
+                response.status(400).send(error)
+            });
+    } else {
+        response.setHeader("Allow", "POST");
+        response.status(405).send("Method Not Allowed")
+    }
 });
